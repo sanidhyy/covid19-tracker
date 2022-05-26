6b0c34f73640cf002c3e4fd1aed88bc5b922d3cd
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-05-26 13:11:42 +0530

    created components needed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 
+import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./App.module.css";
+
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Covid 19 tracker</h1>
+      <div className={styles.container}>
+        <Cards />
+        <CountryPicker />
+        <Chart />
       </div>
     );
   }
