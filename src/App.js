4e77e2451d3c3953b6a5dba42cedf3ad62eaa4fc
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-05-26 13:22:40 +0530

    recieved data from api

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React, { Component } from "react";
 
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
+import { fetchData } from "./api";
 
 export default class App extends Component {
+  async componentDidMount() {
+    const data = await fetchData();
+
+    console.log(data);
+  }
+
   render() {
     return (
       <div className={styles.container}>
